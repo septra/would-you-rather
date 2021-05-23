3b577bf83ce178228463d8929eb28014705cdcf9
Sahil Singh <sahilsingh90@gmail.com>
2021-05-24 03:00:57 +0530

    Update App with routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import 'antd/dist/antd.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Nav from './Nav'
+import Poll from './Poll'
+import NewQuestion from './NewQuestion'
+import Leaderboard from './Leaderboard'
+import User from './User'
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Nav />
+      <Route path='/' exact component={Poll} />
+      <Route path='/add' exact component={NewQuestion} />
+      <Route path='/leaderboard' exact component={Leaderboard} />
+      <Route path='/user' exact component={User} />
+    </Router>
   )
 }
 
