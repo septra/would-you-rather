1ed62b8ffb31fe89c98e9a7d4a183a609b6f1330
Sahil Singh <sahilsingh90@gmail.com>
2021-05-24 03:23:20 +0530

    Add dummy cards to Poll component

--- src/Poll.jsx
+++ src/Poll.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Card, Col, Row } from 'antd';
+import styled from 'styled-components'
+
+
+const StyledCard = styled(Card)`
+    border: 1px solid black;
+`
+
 
 const Poll = () => {
     return (
-        <div>
-            Poll
-        </div>
+      <div style={{ padding: '20px 50px' }}>
+        <Row gutter={8}>
+            <Col span={12}>
+                <StyledCard title="Unanswered" bordered={false}>
+                    StyledCard content
+                </StyledCard>
+            </Col>
+            <Col span={12}>
+                <StyledCard title="Answered" bordered={false}>
+                    StyledCard content
+                </StyledCard>
+            </Col>
+        </Row>
+      </div>
     );
 }
 
