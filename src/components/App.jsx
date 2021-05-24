d9814b4d1207e727ee042b5bcef69a1502fcef27
Sahil Singh <sahilsingh90@gmail.com>
2021-05-25 03:29:48 +0530

    Connect App component loading state with react hooks instead of connect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import Nav from './Nav'
 import Poll from './Poll'
 import NewQuestion from './NewQuestion'
@@ -9,23 +11,31 @@ import Leaderboard from './Leaderboard'
 import Question from './Question'
 import { handleInitialData } from '../actions/shared'
 
-function App(props) {
+function App() {
+
+  const dispatch = useDispatch()
+  const loading = useSelector(state => state.loading)
   
   useEffect(() => {
-    props.dispatch(handleInitialData())
-  })
+      dispatch(handleInitialData())
+  }, [])
 
   return (
     <Router>
       <Nav />
-      <div style={{ padding: '20px 100px' }}>
-        <Route path='/' exact component={Poll} />
-        <Route path='/add' exact component={NewQuestion} />
-        <Route path='/leaderboard' exact component={Leaderboard} />
-        <Route path='/question/:id' component={Question} />
-      </div>
+      {(loading) 
+        ?  <div>
+            <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+          </div>
+        : <div style={{ padding: '20px 100px' }}>
+            <Route path='/' exact component={Poll} />
+            <Route path='/add' exact component={NewQuestion} />
+            <Route path='/leaderboard' exact component={Leaderboard} />
+            <Route path='/question/:id' component={Question} />
+        </div>
+      }
     </Router>
   )
 }
 
-export default connect()(App)
+export default App
