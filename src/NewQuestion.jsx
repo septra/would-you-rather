fe3e6506e3ac47990ca9d708a23503a7672fb8bc
Sahil Singh <sahilsingh90@gmail.com>
2021-05-24 18:29:28 +0530

    Add form view for NewComponent

--- src/NewQuestion.jsx
+++ src/NewQuestion.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Form, Input, Button, Card, Divider } from 'antd';
+import styled from 'styled-components'
+
+const StyledButton = styled(Button)`
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    display: block;
+    border-radius: 5px;
+`
+
+const Div = styled.div`
+    font-weight: 700;
+    line-height: 50px;
+    padding-bottom: 10px
+`
 
 const NewQuestion = () => {
     return (
-        <div>
-            New Question
-        </div>
+        <Card title="Create New Question" bordered={true} hoverable={true}>
+            <Form size="middle">
+                <Div>
+                    Would you rather ...
+                </Div>
+                <Form.Item>
+                    <Input placeholder="Enter Option 1 text" />
+                </Form.Item>
+                <Divider plain>Or</Divider>
+                <Form.Item>
+                    <Input placeholder="Enter Option 2 text" />
+                </Form.Item>
+                <Form.Item>
+                    <StyledButton type="primary">Submit</StyledButton>
+                </Form.Item>
+            </Form>
+        </Card>
     );
 }
 
