fdc1fb2138caf256a50a757dc9796207f05adf34
Sahil Singh <sahilsingh90@gmail.com>
2021-05-26 01:05:17 +0530

    Add logout logic to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
 import loading from './loading'
+import { LOGOUT } from '../actions/authedUser'
 
 export default combineReducers({
   authedUser,
@@ -10,3 +11,6 @@ export default combineReducers({
   questions,
   loading,
 })
+
+// export default (state, action) =>
+//   rootReducer(action.type === LOGOUT ? undefined : state, action);
