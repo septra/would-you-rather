fe78616a0931202e628e3a6c0f375060d29565fb
Sahil Singh <sahilsingh90@gmail.com>
2021-05-25 19:29:23 +0530

    Add authed user functionality with single user automatic auth

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,9 +2,11 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
 import { showLoading, hideLoading } from './loader'
+import { setAuthedUser } from './authedUser'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(setAuthedUser('johndoe'))
     dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux'
-// import authedUser from './authedUser'
+import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
 import loading from './loading'
 
 export default combineReducers({
-// authedUser,
+  authedUser,
   users,
   questions,
   loading,
