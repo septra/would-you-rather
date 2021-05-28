cc2470e866f1f711ce7008d5f6415066787cb984
Sahil Singh <sahilsingh90@gmail.com>
2021-05-29 02:34:04 +0530

    Update user and question states when answerQuestion is dispatched

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions'
+import { ANSWER_QUESTION, RECEIVE_QUESTIONS } from '../actions/questions'
 
 export default function questions(state = {}, action) {
     switch (action.type) {
@@ -6,6 +6,18 @@ export default function questions(state = {}, action) {
             return {
                 ...state,
                 ...action.questions
+            }
+        case ANSWER_QUESTION:
+            return {
+                ...state,
+                [action.questionid] : {
+                    ...state[action.questionid],
+                    [action.option] : {
+                        ...state[action.questionid][action.option],
+                        votes : state[action.questionid][action.option].votes.concat([action.userid])
+ 
+                    }
+                }
             }
         default:
             return state
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS } from '../actions/users'
+import { ANSWER_QUESTION } from '../actions/questions'
 
 export default function users(state = {}, action) {
     switch (action.type) {
@@ -6,6 +7,17 @@ export default function users(state = {}, action) {
             return {
                 ...state,
                 ...action.users
+            }
+        case ANSWER_QUESTION:
+            return {
+                ...state,
+                [action.userid]: {
+                    ...state[action.userid],
+                    answers : Object.assign(
+                        state[action.userid].answers, 
+                        { [action.questionid]: action.option }
+                    )
+                }
             }
         default:
             return state
