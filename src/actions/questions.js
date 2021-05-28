8f7449aa1851ef7c9d931125113f55498f6e4a2a
Sahil Singh <sahilsingh90@gmail.com>
2021-05-29 02:33:33 +0530

    Add answerQuestion action to actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+export function answerQuestion(userid, questionid, option) {
+    return {
+        type: ANSWER_QUESTION,
+        userid,
+        questionid,
+        option
     }
 }
